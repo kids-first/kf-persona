a4cde760e2e0843e206b769d5d832a330593ee2f
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-09-27 15:40:41 -0400

    Switch filter map order

--- src/schema/User.js
+++ src/schema/User.js
@@ -34,7 +34,7 @@ export default {
     // research interests
     website: "String",
     googleScholarId: "String",
-    interests: {type: ["String"], set: interests => interests.map(interest => interest.toLowerCase()).filter((interest, i, arr) => arr.indexOf(interest) === i)},
+    interests: {type: ["String"], set: interests => interests.filter((interest, i, arr) => arr.indexOf(interest) === i).map(interest => interest.toLowerCase())},
     twitter: "String",
     facebook: "String",
     github: "String",
