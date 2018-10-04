e5673e436e41b532c2883033d416acd772ff3d54
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-10-04 13:30:10 -0400

    Fix migration

--- migrations/20180927201954-lowercase_user_interests.js
+++ migrations/20180927201954-lowercase_user_interests.js
@@ -1,32 +1,45 @@
-'use strict';
+"use strict";
+
+const ObjectID = require("mongodb").ObjectID;
 
 module.exports = {
-  up(db, next) {
-    // update records    
-    const users = db.collection('users');
-    users.find({}).snapshot().forEach(
-      doc => {
-        const newInterests = doc.interests.map(interest => {
-          return interest.toLowerCase().trim()
-        })
-        .filter((interest, i, arr) => arr.indexOf(interest) === i);
-        
-        doc.interests = newInterests;
+  up(db) {
+    const users = db.collection("users");
 
-        users.replaceOne({_id: doc._id}, doc)
-        .then(msg => { 
-          console.log('replace success', msg.result);     
-          next();
-        })
-        .catch(err => console.warn('replace failed', err));
-      }
-    );
+    // Update using foreach because we need to edit based on current value
+    const updates = [];
+    return users
+      .find({})
+      .forEach(doc =>
+        updates.push(
+          new Promise((res, rej) => {
+            const newInterests = doc.interests
+              .map(interest => interest.toLowerCase().trim())
+              .filter(
+                (interest, i, arr) => interest && arr.indexOf(interest) === i
+              );
 
+            return users
+              .updateOne(
+                { _id: doc._id },
+                { $set: { interests: newInterests } }
+              )
+              .then(res)
+              .catch(err => rej({ failedDoc: doc, err }));
+          })
+        )
+      )
+      .then(() =>
+        Promise.all(updates)
+          .then(results => console.log("Migration successful"))
+          .catch(err => {
+            console.log("Migration failed", err);
+            throw Error();
+          })
+      );
   },
 
-  down(db, next) {
-    console.warn('No down migration');
-    next();
+  down(db) {
+    console.warn("No down migration");
   }
-
 };
