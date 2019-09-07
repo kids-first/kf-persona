607e0877c7d0a38b8df49b56b4b88caed7de456c
jecos <jecos.crsj@gmail.com>
2019-09-06 20:44:30 -0400

    Send message to sqs

--- src/endpoints/push.js
+++ src/endpoints/push.js
@@ -1,11 +1,25 @@
-export default (model, sqs, queueName) => async (req, res) => {
-    if (!queueName)
-        return Promise.resolve(res.status(500).json({message: 'There is no queue defined!'}))
+const sendMessage = (sqs, queueUrl) => user => {
+    const params = {MessageBody: user, QueueUrl: queueUrl};
+    sqs.sendMessage(params).promise()
+};
+
+export default (model, sqs, queueUrl) => async (req, res) => {
+    if (!queueUrl)
+        return Promise.resolve(res.status(500).json({message: 'Queue URL is not defined!'}));
+
     await model.find({})
         .exec()
-        .then(function (users) {
-            return res.json(users);
-        }).catch(function (err) {
-            return res.status(500).json(err);
+        .then((users) => {
+            Promise.all(users.map(u => sendMessage(sqs, queueUrl)(u)));
+        })
+        .then((results) => {
+                res.send({
+                    result: 'ok',
+                    nbElementPushed: results.length,
+                    messageIds: results.map(data => data.MessageId)
+                });
+            }
+        ).catch(err => {
+            res.status(500).json(err);
         });
 };
