f1d5d8589e98ae317ef96d65e9283b96cb90e156
evans-g-crsj <eg.crsj@gmail.com>
2021-03-02 10:58:43 -0500

    Clean up

--- src/Models/memberReport.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-import { memberReport } from '../schema/MemberReport';
-import { MODEL_MEMBER_REPORT_NAME } from '../constants';
-
-export const getMemberReportModel = () =>
-  mongoose.model(MODEL_MEMBER_REPORT_NAME, memberReport);
